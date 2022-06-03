e0f285f6b34fe00086be18c93bea2b98d045edbe
MujtabaAbdullahi <mujtabamuhammadi037@gmail.com>
2022-06-03 01:44:29 -0800

    My gym application

---
+++ GYMUI/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GYMUI.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
